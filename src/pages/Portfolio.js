5c95b9d4d7e4ac14a7105719a1034c628288d562
MABAROCELA <marbarocela@gmail.com>
2023-12-22 11:37:10 -0300

    third commit

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -6,7 +6,7 @@ function Home() {
     return (
         <>
             <div className='descripcion-container'>
-                <p>Recently graduated Full Stack Developer. Looking for her first job. Still managed to make a quite fair website!</p>
+                <p>Recently graduated Full Stack Developer. Looking for her first job. Still managed to make quite a decent website!</p>
                 <Link className="perita-link" target="_blank" rel="noopener noreferrer" to='https://perita-front.vercel.app'>- CHECK THAT DEPLOY -</Link>
             </div>
         </>
